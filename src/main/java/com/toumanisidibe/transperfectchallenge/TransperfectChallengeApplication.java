a7fa9bb72cf39c920596f0000eea59babd0a72d2
Toumani <toumani49@gmail.com>
2023-04-05 16:37:32 +0000

    First commit

---
+++ src/main/java/com/toumanisidibe/transperfectchallenge/TransperfectChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.toumanisidibe.transperfectchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransperfectChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransperfectChallengeApplication.class, args);
+	}
+
+}
